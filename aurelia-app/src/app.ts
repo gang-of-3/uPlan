01e19361d3e495b5420e5205804d68e50a10fb4c
Greg Warchol <gew33@drexel.edu>
2019-05-26 14:42:30 -0400

    Cleaned up routes and logon page

--- aurelia-app/src/app.ts
+++ aurelia-app/src/app.ts
@@ -5,16 +5,9 @@ export class App {
   public router: Router;
 
   public configureRouter(config: RouterConfiguration, router: Router) {
-    console.log('im alive');
-    config.title = 'Aurelia';
+    config.title = 'uPlan';
     config.options.pushState = true;
     config.map([
-      {
-        route: ['', 'welcome'],
-        name: 'welcome',
-        moduleId: PLATFORM.moduleName('./welcome'),
-        title: 'Welcome'
-      },
       {
         route: 'calendar',
         name: 'calendar',
@@ -28,7 +21,7 @@ export class App {
         title: 'TODO'
       },
       {
-        route: 'logon',
+        route: ['', 'logon'],
         name: 'logon',
         moduleId: PLATFORM.moduleName('./logon/logon'),
         title: 'Logon'
