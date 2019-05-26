9f34e500da549e816cc77868c6d240f6e8cf02fb
Greg Warchol <gew33@drexel.edu>
2019-05-25 20:11:46 -0400

    Removed provided files, added routing

--- aurelia-app/src/app.ts
+++ aurelia-app/src/app.ts
@@ -5,28 +5,27 @@ export class App {
   public router: Router;
 
   public configureRouter(config: RouterConfiguration, router: Router) {
+    console.log('im alive');
     config.title = 'Aurelia';
+    config.options.pushState = true;
     config.map([
       {
         route: ['', 'welcome'],
         name: 'welcome',
         moduleId: PLATFORM.moduleName('./welcome'),
-        nav: true,
         title: 'Welcome'
       },
       {
-        route: 'users',
-        name: 'users',
-        moduleId: PLATFORM.moduleName('./users'),
-        nav: true,
-        title: 'Github Users'
+        route: 'calendar',
+        name: 'calendar',
+        moduleId: PLATFORM.moduleName('./calendar/calendar'),
+        title: 'Calendar'
       },
       {
-        route: 'child-router',
-        name: 'child-router',
-        moduleId: PLATFORM.moduleName('./child-router'),
-        nav: true,
-        title: 'Child Router'
+        route: 'todo',
+        name: 'todo',
+        moduleId: PLATFORM.moduleName('./todo/todo'),
+        title: 'TODO'
       }
     ]);
 
