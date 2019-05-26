48fbffcc3bf12b5036c64412f982a74e1df575f1
Greg Warchol <gew33@drexel.edu>
2019-05-26 19:40:04 -0400

    Added month browsing

--- aurelia-app/src/calendar/calendar.ts
+++ aurelia-app/src/calendar/calendar.ts
@@ -1,23 +1,51 @@
 export class Calendar{
   dayNames: String[];
   monthNames: String[];
-  month: String[][];
+  calendar: String[][];
   monthName:String;
-  year: String;
+  activeYear: number;
+  activeMonth:number;
 
   constructor(){
     this.dayNames = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
     this.monthNames = ['January','February','March','April','May','June','July', 'August','September','October','November','December'];
     var currentMonth = this.getCurrentMonth();
-    this.monthName = this.monthNames[currentMonth];
-    this.month = this.buildMonth(currentMonth);
-    this.year = this.getCurrentYear();
+    var currentYear = this.getCurrentYear();
+
+    this.setupCalendar(currentMonth, currentYear);
   }
 
-  buildMonth(month){
+  previousMonth(){
+    var previousMonth = this.activeMonth - 1, previousYear = this.activeYear;
+    if(this.activeMonth == 0){
+      previousMonth = 11;
+      previousYear -= 1;
+    }
 
-    var totalDaysInMonth = this.calculateTotalDaysInMonth(month);
-    var firstWeekdayOfMonth = this.calculateFirstWeekdayOfMonth(month);
+    this.setupCalendar(previousMonth, previousYear);
+  }
+
+  nextMonth(){
+    var nextMonth = this.activeMonth + 1, nextYear = this.activeYear;
+    if(this.activeMonth == 11){
+      nextMonth = 0;
+      nextYear += 1;
+    }
+
+    this.setupCalendar(nextMonth, nextYear);
+  }
+
+  setupCalendar(month, year){
+    this.activeMonth = month;
+    this.activeYear = year;
+    this.monthName = this.monthNames[month];
+    this.calendar = this.buildMonth(month, year);
+  }
+
+  private buildMonth(month, year){
+
+    var totalDaysInMonth = this.calculateTotalDaysInMonth(month, year);
+    var firstWeekdayOfMonth = this.calculateFirstWeekdayOfMonth(month, year);
     var numWeeksInMonth = this.calculateNumberWeeksInMonth(totalDaysInMonth,firstWeekdayOfMonth);
 
     var i,j;
@@ -39,7 +67,7 @@ export class Calendar{
   private getCurrentYear(){
     var date = new Date();
     var currentYear = date.getFullYear();
-    return currentYear.toString();
+    return currentYear;
   }
 
   private getCurrentMonth(){
@@ -54,16 +82,18 @@ export class Calendar{
     return numWeeksInMonth;
   }
 
-  private calculateTotalDaysInMonth(month){
+  private calculateTotalDaysInMonth(month, year){
     var date = new Date();
+    date.setFullYear(year);
     date.setMonth(month+1);
     date.setDate(0);
 
     return date.getDate();
   }
 
-  private calculateFirstWeekdayOfMonth(month){
+  private calculateFirstWeekdayOfMonth(month, year){
     var date = new Date();
+    date.setFullYear(year);
     date.setMonth(month);
     date.setDate(1);
 
