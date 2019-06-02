ce0225500d5755d9ff16723ef354020bc3d69df9
Greg Warchol <gew33@drexel.edu>
2019-06-02 14:25:45 -0400

    Fixed date issue

--- aurelia-app/src/calendar/itinerary/itinerary-service.ts
+++ aurelia-app/src/calendar/itinerary/itinerary-service.ts
@@ -1,32 +1,31 @@
 import {CalendarItem} from "./calendar-item";
 
 export class ItineraryService {
-  events:CalendarItem[];
+  events: CalendarItem[];
 
   constructor() {
     this.events = [];
   }
 
-  removeItineraryItem(id:String){
-    this.events = this.events.filter((item)=>{
+  removeItineraryItem(id: String) {
+    this.events = this.events.filter((item) => {
       return item.id != id;
     });
   }
 
-  addItineraryItem(item:CalendarItem){
+  addItineraryItem(item: CalendarItem) {
     this.events.push(item);
   }
 
-  setFullItinerary(events){
+  setFullItinerary(events) {
     this.events = events;
   }
 
-  findItinerary( year, month, day){
-    return this.events ? this.events.filter((item:CalendarItem) => {
-      var dateTime:Date = item.dateTime;
-
+  findItinerary(year, month, day) {
+    return this.events ? this.events.filter((item: CalendarItem) => {
+      var dateTime: Date = new Date(item.dateTime);
       return dateTime.getFullYear() === year && dateTime.getMonth() === month && dateTime.getDate() === day;
-    }): [];
+    }) : [];
   }
 
 }
